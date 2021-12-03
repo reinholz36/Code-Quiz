6be094a834ffc0234b99175cfe8739a4fcdc09d1
reinholz36 <dreinholz28@gmail.com>
2021-12-03 16:05:55 -0500

    started adding styling

---
+++ assets/js/script.js
@@ -0,0 +1,4 @@
+const quizBox = document.getElementById("quiz");
+const resultsBox = document.getElementById("results");
+const submitBtn = document.getElementById("submit")
+console.log("Testing");
