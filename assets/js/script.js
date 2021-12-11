95eda5e7dffe159f739c0cd9373aac0c438a92bd
reinholz36 <dreinholz28@gmail.com>
2021-12-11 18:02:36 -0500

    completed 20 question array

--- assets/js/script.js
+++ assets/js/script.js
@@ -88,102 +88,102 @@ var questionArray = [
         correct: 0
     },
     
-    //Add different questions to
     {
-        question: "What is the correct syntax for referring to an exteranl script called script.js ?",
-        choices: ['<script src="scrip.js">', '<script name="script.js">', '<script href="script.js">', '<"script.js">'],
+        question: "What is DOM?",
+        choices: ["Digital Origin Main", "Document Object Model", "Document Object Motel", "Dial Open Modem"],
+        correct: 1
+    },
+    
+    {
+        question: 'How do you write "Help, I am trapped in a coding quiz!" in an alert box?',
+        choices: ['msg("Help, I am trapped in a coding quiz!");', 'alertBox("Help, I am trapped in a coding quiz!");', 'msgBox("Help, I am trapped in a coding quiz!");', 'alert("Help, I am trapped in a coding quiz!");'],
+        correct: 3
+    },
+    
+    {
+        question: "How do you create a function in JavaScript?",
+        choices: ["<myFunction>()", "function = myFunction()", "function myFunction()", "function:myFunction()"],
+        correct: 2
+    },
+    
+    {
+        question: 'How do you call a function named "myFunction"?',
+        choices: ["myFunction()", "call{myFunction}", "return = myFunction;", "myFunction{}"],
         correct: 0
+    },
+    
+    {
+        question: "How do you write an IF statement in JavaScript?",
+        choices: ["if i = 5 then", "if i = 5", "if i == 5 then", "if (i==5)"],
+        correct: 3
+    },
+    
+    {
+        question: "How do you write an IF statement for executin some code if 'i' is NOT equal to 5?",
+        choices: ["if (i! = 5)", "if i <> 5", "if (i <> 5)", "if i = !5 then"],
+        correct: 0
+    },
+    
+    {
+        question: "How does a WHILE loop start?",
+        choices: ["while (i<= 10;i++)", "while i = 1 to 10", "while (i <= 10) ", "while {i <= 10}"],
+        correct: 2
+    },
+    
+    {
+        question: "How does a FOR loop start?",
+        choices: ["for (i <= 5; i++)", "for (i = 0; i <= 5; i++)", "for i = 1 to 5", "for (i = 0; i <= 5)"],
+        correct: 1
+    },
+    
+    {
+        question: "How can you add a comment in a JavaScript?",
+        choices: ["<!--This is a comment-->", "<This is a comment>", "|This is a comment|", "//This is a comment"],
+        correct: 3
+    },
+    
+    {
+        question: "How do you round the number 7.25, to the nearest integer?",
+        choices: ["rnd(7.25)", "Math.round(7.25)", "round(7.25)", "Math.rnd(7.25)"],
+        correct: 1
+    },
+    
+    {
+        question: "How do you find the number with the highest value of x and y?",
+        choices: ["Math.max(x, y)", "Math.ceil(x, y)", "ceil(x, y)", "top(x, y)"],
+        correct: 0
+    },
+    
+    {
+        question: "Which operator is used to assign a value to a variable?",
+        choices: ["  *  ", "  X  ", "  =  ", "  -  "],
+        correct: 2
+    },
+    
+    {
+        question: 'What is the output of 10+20+"30" in JavaScript?',
+        choices: ["3030", "60", "30", "10+20"],
+        correct: 0
+    },
+    
+    {
+        question: "What is the use of Map object in JavaScript?",
+        choices: ["map true or false", "map the DOM", "map functions to var", "map keys to values"],
+        correct: 3
+    },
+    
+    {
+        question: "Which operator checks equality, and data type i.e. a value must be of the same type?",
+        choices: [" --- ", " == ", " = ", " === "],
+        correct: 3
     },
 
     {
-        question: "What is the correct syntax for referring to an exteranl script called script.js ?",
-        choices: ['<script src="scrip.js">', '<script name="script.js">', '<script href="script.js">', '<"script.js">'],
-        correct: 0
+        question: "Which of the following is not a pop-up box availale in JavaScript?",
+        choices: ["alert()", "confirm()", "function()", "prompt()"],
+        correct: 2
     },
 
-    {
-        question: "What is the correct syntax for referring to an exteranl script called script.js ?",
-        choices: ['<script src="scrip.js">', '<script name="script.js">', '<script href="script.js">', '<"script.js">'],
-        correct: 0
-    },
-
-    {
-        question: "What is the correct syntax for referring to an exteranl script called script.js ?",
-        choices: ['<script src="scrip.js">', '<script name="script.js">', '<script href="script.js">', '<"script.js">'],
-        correct: 0
-    },
-
-    {
-        question: "What is the correct syntax for referring to an exteranl script called script.js ?",
-        choices: ['<script src="scrip.js">', '<script name="script.js">', '<script href="script.js">', '<"script.js">'],
-        correct: 0
-    },
-
-    {
-        question: "What is the correct syntax for referring to an exteranl script called script.js ?",
-        choices: ['<script src="scrip.js">', '<script name="script.js">', '<script href="script.js">', '<"script.js">'],
-        correct: 0
-    },
-
-    {
-        question: "What is the correct syntax for referring to an exteranl script called script.js ?",
-        choices: ['<script src="scrip.js">', '<script name="script.js">', '<script href="script.js">', '<"script.js">'],
-        correct: 0
-    },
-
-    {
-        question: "What is the correct syntax for referring to an exteranl script called script.js ?",
-        choices: ['<script src="scrip.js">', '<script name="script.js">', '<script href="script.js">', '<"script.js">'],
-        correct: 0
-    },
-
-    {
-        question: "What is the correct syntax for referring to an exteranl script called script.js ?",
-        choices: ['<script src="scrip.js">', '<script name="script.js">', '<script href="script.js">', '<"script.js">'],
-        correct: 0
-    },
-
-    {
-        question: "What is the correct syntax for referring to an exteranl script called script.js ?",
-        choices: ['<script src="scrip.js">', '<script name="script.js">', '<script href="script.js">', '<"script.js">'],
-        correct: 0
-    },
-
-    {
-        question: "What is the correct syntax for referring to an exteranl script called script.js ?",
-        choices: ['<script src="scrip.js">', '<script name="script.js">', '<script href="script.js">', '<"script.js">'],
-        correct: 0
-    },
-
-    {
-        question: "What is the correct syntax for referring to an exteranl script called script.js ?",
-        choices: ['<script src="scrip.js">', '<script name="script.js">', '<script href="script.js">', '<"script.js">'],
-        correct: 0
-    },
-
-    {
-        question: "What is the correct syntax for referring to an exteranl script called script.js ?",
-        choices: ['<script src="scrip.js">', '<script name="script.js">', '<script href="script.js">', '<"script.js">'],
-        correct: 0
-    },
-
-    {
-        question: "What is the correct syntax for referring to an exteranl script called script.js ?",
-        choices: ['<script src="scrip.js">', '<script name="script.js">', '<script href="script.js">', '<"script.js">'],
-        correct: 0
-    },
-
-    {
-        question: "What is the correct syntax for referring to an exteranl script called script.js ?",
-        choices: ['<script src="scrip.js">', '<script name="script.js">', '<script href="script.js">', '<"script.js">'],
-        correct: 0
-    },
-
-    {
-        question: "What is the correct syntax for referring to an exteranl script called script.js ?",
-        choices: ['<script src="scrip.js">', '<script name="script.js">', '<script href="script.js">', '<"script.js">'],
-        correct: 0
-    },
 ];
 
 
