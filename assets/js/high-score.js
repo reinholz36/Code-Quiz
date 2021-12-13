9ad436d0cba5447feeef14d5da13ff8ad72baa33
reinholz36 <dreinholz28@gmail.com>
2021-12-12 23:07:38 -0500

    additional file clean

--- assets/js/high-score.js
+++ assets/js/high-score.js
@@ -8,15 +8,15 @@ topHeader.appendChild(countDown);
 //this sets up a pull from local storage or loads a blank array if nothing is in storage
 var savedScores = JSON.parse(localStorage.getItem("scoreArray")) ?? [];
     console.log("savedScores", savedScores);
-    
-window.onload = () => {
 
+window.onload = () => {
+//sorts array based on score
     var sortedValues = savedScores.sort((a,b)=>{
         return b.score - a.score
         
     })
 
-
+//populates the High-Score page
     for (var i = 0; i < sortedValues.length; i++) {
         var pulledScore = sortedValues[i].score 
         var pulledName = sortedValues[i].name 
@@ -27,7 +27,4 @@ window.onload = () => {
         
     }
 }
-        //pulls scores and initals from local storage 
-        //sorts scores and intials by high score
-        //displays sorted to table on high-score.html
-        //(optional) maybe include clear high score button
+
